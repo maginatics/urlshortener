maginatics/urlshortener

commit 1e2e5163a1f6aad3b232f13c875c7c9bfb9e1122

Prefer URI over URL
Modern APIs prefer the former since the latter has undesirable
behavior, such as resolving the hostname to IP address in URL.equals.
Further URI has conveniences like URI.create(String).

src/main/java/urlshortener/BitlyUrlShortener.java
@@ -16,7 +16,7 @@
 
 package urlshortener;
 
-import java.net.URL;
+import java.net.URI;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -53,7 +53,7 @@ final class BitlyUrlShortener implements UrlShortener {
     }
 
     @Override
-    public URL shorten(final URL longUrl) {
+    public URI shorten(final URI longUrl) {
         ShortenResponse response = bitly.shorten(accessToken, longUrl);
         // If an error happened, returned the original as-is
         if (response.statusCode != HTTP_OK) {
@@ -61,14 +61,14 @@ final class BitlyUrlShortener implements UrlShortener {
                 longUrl, response.statusCode);
             return longUrl;
         }
-        return response.data.url;
+        return response.data.uri;
     }
 
     interface BitlyService {
         // See http://dev.bitly.com/links.html#v3_shorten
         @GET("/v3/shorten")
         ShortenResponse shorten(@Query("access_token") String accessToken,
-                                @Query("longUrl") URL longUrl);
+                                @Query("longUrl") URI longUrl);
     }
 
     private static final class ShortenResponse {
@@ -82,7 +82,7 @@ final class BitlyUrlShortener implements UrlShortener {
         }
 
         static final class Data {
-            private URL url;
+            private URI uri;
 
             // Empty constructor for gson
             Data() {

src/main/java/urlshortener/GoogleUrlShortener.java
@@ -16,7 +16,7 @@
 
 package urlshortener;
 
-import java.net.URL;
+import java.net.URI;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -43,7 +43,7 @@ final class GoogleUrlShortener implements UrlShortener {
     }
 
     @Override
-    public URL shorten(final URL longUrl) {
+    public URI shorten(final URI longUrl) {
         try {
             return google.shorten(apiKey, new Payload(longUrl)).id;
         } catch (RetrofitError e) {
@@ -61,10 +61,10 @@ final class GoogleUrlShortener implements UrlShortener {
     }
 
     private static final class Payload {
-        private URL id;
-        private URL longUrl;
+        private URI id;
+        private URI longUrl;
 
-        Payload(final URL longUrl) {
+        Payload(final URI longUrl) {
             this.id = null;
             this.longUrl = longUrl;
         }

src/main/java/urlshortener/IdentityUrlShortener.java
@@ -16,12 +16,12 @@
 
 package urlshortener;
 
-import java.net.URL;
+import java.net.URI;
 
 /** A UrlShortener that simply returns the long URL as-is. */
 final class IdentityUrlShortener implements UrlShortener {
     @Override
-    public URL shorten(final URL longUrl) {
+    public URI shorten(final URI longUrl) {
         return longUrl;
     }
 }

src/main/java/urlshortener/UrlShortener.java
@@ -16,7 +16,7 @@
 
 package urlshortener;
 
-import java.net.URL;
+import java.net.URI;
 
 /** Minimal API for URL Shorteners. */
 public interface UrlShortener {
@@ -28,5 +28,5 @@ public interface UrlShortener {
      *         This is a best-effort interface and implementers may return the
      *         longUrl in case of errors.
      */
-    URL shorten(URL longUrl);
+    URI shorten(URI longUrl);
 }

src/test/java/urlshortener/UrlShortenersTest.java
@@ -18,8 +18,7 @@ package urlshortener;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.URI;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assume.assumeTrue;
@@ -27,21 +26,13 @@ import org.junit.Test;
 
 /** Tests for UrlShorteners. */
 public final class UrlShortenersTest {
-    private static final URL longUrl;
+    private static final URI longUrl = URI.create(
+        "http://127.0.0.1/some/random/url");
 
-    static {
-        URL tmpUrl = null;
-        try {
-            tmpUrl = new URL("http://127.0.0.1/some/random/url");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        longUrl = tmpUrl;
-    }
-
-    private static void validateRedirectedUrl(final URL shortUrl)
+    private static void validateRedirectedUrl(final URI shortUrl)
             throws IOException {
-        HttpURLConnection conn = (HttpURLConnection) shortUrl.openConnection();
+        HttpURLConnection conn = (HttpURLConnection) shortUrl.toURL()
+            .openConnection();
         conn.setInstanceFollowRedirects(false);
         conn.connect();
         assertThat(longUrl.toString()).isEqualTo(conn.getHeaderField(
@@ -55,7 +46,7 @@ public final class UrlShortenersTest {
         assumeTrue(accessToken != null);
         UrlShortener urlShortener = UrlShorteners.bitlyUrlShortener(
             accessToken);
-        URL shortUrl = urlShortener.shorten(longUrl);
+        URI shortUrl = urlShortener.shorten(longUrl);
         validateRedirectedUrl(shortUrl);
     }
 
@@ -64,7 +55,7 @@ public final class UrlShortenersTest {
         String apiKey = System.getProperty("urlshortener.googleApiKey");
         assumeTrue(apiKey != null);
         UrlShortener urlShortener = UrlShorteners.googleUrlShortener(apiKey);
-        URL shortUrl = urlShortener.shorten(longUrl);
+        URI shortUrl = urlShortener.shorten(longUrl);
         validateRedirectedUrl(shortUrl);
     }
 

commit cec957105193cc185b5f72b521776bb7a3739da7

Fix bit.ly breakage introduced in 1e2e5163

src/main/java/urlshortener/BitlyUrlShortener.java
@@ -61,7 +61,7 @@ final class BitlyUrlShortener implements UrlShortener {
                 longUrl, response.statusCode);
             return longUrl;
         }
-        return response.data.uri;
+        return response.data.url;
     }
 
     interface BitlyService {
@@ -82,7 +82,7 @@ final class BitlyUrlShortener implements UrlShortener {
         }
 
         static final class Data {
-            private URI uri;
+            private URI url;
 
             // Empty constructor for gson
             Data() {